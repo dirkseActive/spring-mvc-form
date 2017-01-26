c677be83061475f7bc6652b6c0cb4fd002fbdc40
dirkseActive <odysseusontheocean@yahoo.com>
2017-01-26 17:17:17 -0500

    Adding classes

---
+++ src/main/java/com/mkyong/form/web/UserController.java
@@ -0,0 +1,15 @@
+package com.mkyong.form.web;
+
+
+
+/**
+ * 
+ * @author ME.  1/26/2017 MVC tutorial
+ * Using:  mkyong.com/spring-mvc/spring-mvc-form-handling-example/
+ *
+ */
+
+@Controller
+public class UserController {
+
+}
